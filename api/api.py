e730c7c7748cc298370f3806c7e7ad8284d463eb
qfeuilla <quentinfeuillademontixi42@gmail.com>
2023-10-12 15:57:24 +0200

    feat: update ChatNode

--- api/api.py
+++ api/api.py
@@ -8,15 +8,21 @@ from flask_cors import CORS
 import pandas as pd
 import tiktoken
 from platform import uname
-
+import time
+from dataclasses import dataclass
 
 def in_wsl() -> bool:
     return 'microsoft-standard' in uname().release
 
 price_table = {
     "gpt-4" : [0.03, 0.06],
+    "gpt-4-0613": [0.03, 0.06],
     "gpt-4-32k": [0.06, 0.12],
-    "gpt-3.5-turbo": [0.002, 0.002],
+    "gpt-4-32k-0613": [0.06, 0.12],
+    "gpt-3.5-turbo": [0.0015, 0.002],
+    "gpt-3.5-turbo-16k": [0.003, 0.004],
+    "gpt-3.5-turbo-0613": [0.0015, 0.002],
+    "gpt-3.5-turbo-16k-0613": [0.003, 0.004],
     "text-davinci-003": [0.02, 0.02],
     "text-davinci-002": [0.02, 0.02],
     "davinci": [0.02, 0.02],
@@ -24,8 +30,13 @@ price_table = {
 
 is_chat = {
     "gpt-4" : True,
+    "gpt-4-0613": True,
     "gpt-4-32k": True,
+    "gpt-4-32k-0613": True,
     "gpt-3.5-turbo": True,
+    "gpt-3.5-turbo-16k": True,
+    "gpt-3.5-turbo-0613": True,
+    "gpt-3.5-turbo-16k-0613": True,
     "text-davinci-003": False,
     "text-davinci-002": False,
     "davinci": False,
@@ -36,27 +47,47 @@ openai.api = os.getenv("OPENAI_API_KEY")
 
 save_Lock = Lock()
 save_exp_Lock = Lock()
+@dataclass
+class Prompt:
+    def __init__(self, content: str, role: str, **kwargs):
+        self.content = content
+        self.role = role
+        self.kwargs = kwargs
+    
+    def __getattribute__(self, __name: str) -> Any:
+        if __name == "content":
+            return self.content
+        if __name == "role":
+            return self.role
+        return ""
 
 class ChatNode:
     def __init__(self, role: str, content: str):
-        self.role = role # either "system", "user", or "assistant"
-        self.content = content # the content of the message
-        self.children: List[ChatNode] = [] # a list of ChatNode objects
-        self.parent: Optional[ChatNode] = None # the parent node
+        self.role = role  # either "system", "user", or "assistant"
+        self.content = content  # the content of the message
+        self.children: List[ChatNode] = []  # a list of ChatNode objects
+        self.parent: Optional[ChatNode] = None  # the parent node
 
-    def complete(self, model: str = "gpt-3.5-turbo", temperature: float = 0.7, max_tokens=512, is_chat: bool = True, **kwargs):
+    def complete(
+        self,
+        model: str = "gpt-3.5-turbo",
+        temperature: float = 0.7,
+        max_tokens=512,
+        is_chat: bool = True,
+        **kwargs,
+    ):
         # append the completion of the current branch to the child
-        messages = self.get_messages() # get the messages from the root to this node
+        messages = self.get_messages()  # get the messages from the root to this node
         retry = 3
-        while (retry):
+        while retry:
             try:
-                if (is_chat):
+                if is_chat:
                     response = openai.ChatCompletion.create(
                         model=model,
                         messages=messages,
                         temperature=temperature,
                         max_tokens=max_tokens,
-                        **kwargs
+                        **kwargs,
                     )
                     retry = 0
                     message = response["choices"][0]["message"]
@@ -67,16 +98,23 @@ class ChatNode:
                         prompt="\n".join([m["content"] for m in messages]),
                         temperature=temperature,
                         max_tokens=max_tokens,
-                        **kwargs
+                        **kwargs,
                     )
                     retry = 0
                     message = response["choices"][0]["text"]
                     child = ChatNode("assistant", message)
             except Exception as e:
-                # If last pass then raise the error.
-                if (retry == 1):
+                time.sleep(15)
+                # If last try then raise the error.
+                print(f"Warning: {e}")
+                if retry == 1:
                     raise e
                 retry -= 1
+        self.children.append(child)
+        child.parent = self
+        return child
+
+    def add_child(self, child):
         self.children.append(child)
         child.parent = self
         return child
@@ -86,13 +124,22 @@ class ChatNode:
         messages: List[dict] = []
         node = self
         while node:
-            messages.append({
-                "role": node.role,
-                "content": node.content
-            })
+            messages.append({"role": node.role, "content": node.content})
             node = node.parent
         messages.reverse()
         return messages
+
+    def get_root(self):
+        node = self
+        while node.parent:
+            node = node.parent
+        return node
+
+    def get_last_child(self):
+        node = self
+        while len(node.children) > 0:
+            node = node.children[-1]
+        return node
 
 app = Flask(__name__)
 
